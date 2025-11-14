ac87099954cd99ce264ed450b2d28e3bf520bd8e
r567tw <r567tw@gmail.com>
2025-11-14 19:26:53 +0000

    Build: (b4f27e9) Add GitHub Actions workflow and update README

--- src/config.ts
+++
@@ -1,2 +0,0 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export const BASE_URL = import.meta.env.VITE_BASE_URL;
--- src/types.ts
+++
@@ -1,4 +0,0 @@
-export type ShortUrl = {
-  short: string;
-  url: string;
-};
